c1f8c758d2f279c9eda8944b7bc6c53990a6cf4f
kekoawong <kwong6@nd.edu>
2021-03-16 01:26:34 -0400

    Added degree

--- find_roots.py
+++ find_roots.py
@@ -2,7 +2,9 @@
 import numpy as np 
   
 # Enter the coefficients of the polymial into the array 
-coeff = [1,3,2,2]
+coeff = [1,3,4,3,0,-1,-2,0,0]
+deg = len(coeff) - 1
+print("Degree: " + str(deg))
 # round the roots
 for num in np.roots(coeff):
     num = round(num.real, 3) + round(num.imag, 3) * 1j
